ce0bd9beab33273cb264421bf35eec3cc98ee76c
Amalia <amaliak@stanford.edu>
2015-03-30 17:45:00 -0700

    Added routine to read input file & to plot results

--- FORWARD/main.m
+++ FORWARD/main.m
@@ -1,18 +1,20 @@
-% main
-clear all;
-clc;
-close all;
+function [da,fw]=main(filename)
+% clear all;
+% clc;
+% close all;
 % function [sol,stats] = main(filename)
 % input: filename of input selected by users
 
 % Convert information from txt into a structure param
 % param = readINFILE(filename);
-param.model = 'Saetrom';
-param.method = 'KF';
-param.x_std = 20;
+% param.model = 'Saetrom';
+% param.method = 'KF';
+% param.x_std = 20;
+% param.obsstd = 1; % observation STD
+% param.nt = 10; % total data assimilation step
+param=get_prm(filename);
 param.kernel = @(h) param.x_std.*exp(-(h./(20/3)));
-param.obsstd = 1; % observation STD
-param.nt = 10; % total data assimilation step
+
 % TODO: add other parameters specifed by users:
 % total simulation step
 
@@ -27,7 +29,14 @@ da = selectDA(param,fw);
 % TODO: check if fw and da contain all the necessary properties and functions after initialization
 rng(100);
 % for each data asismilation step
-for i = 1:1
+
+% Plot initial condition
+figure;
+subplot(2,1,1)
+plotstate(da,fw)
+title('Initial condition at step 0');
+
+for i = 1:param.nt
     %% Run truemodel to produce true state xt and data z, save as property of fw
     % call f and h to update state and observation
     fw.step = i; % mandatory for Saetrom case
@@ -46,6 +55,14 @@ for i = 1:1
     da.update(fw,fw.zt);
     
     % save fw and da as snapshots for analysis
+end
+
+%% plot final state
+subplot(2,1,2)
+plotstate(da,fw)
+title('CSKF mean and STD at step 10');
+
+
 end
 
 % clear da
